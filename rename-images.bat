05b3238cddd5473d44abe6d6d965839eb1bc0703
Isiri2023 <isiri.seeds@gmail.com>
2026-01-13 23:00:15 +0530

    heroupdated19

---
+++ rename-images.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Rename images with professional names
+REM Image 1: Field team cultivating tomato plants
+ren "ChatGPT Image Jan 11, 2026, 02_16_26 PM.png" "field-cultivation-team.jpg" 2>nul
+
+REM Image 2: Aerial view of organized crop rows
+ren "Image Jan 11, 2026, 02_16_26 PM.png" "field-rows-aerial.jpg" 2>nul
+
+echo Images renamed successfully!
+pause
